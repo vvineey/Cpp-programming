3f48ebd1107d35ecc0b895b78edf40d015e0d753
vvineey <vvin506@naver.com>
2026-06-14 16:06:46 +0900

    feat : 기말고사 문제 1 구현

---
+++ FinalExamAssignment/problem1.cpp
@@ -0,0 +1,28 @@
+#include <climits>
+#include <iostream>
+using namespace std;
+
+int main() {
+    unsigned int firstNumber;
+    unsigned int secondNumber;
+
+    cout << "첫 번째 숫자를 입력하세요: ";
+    cin >> firstNumber;
+
+    cout << "두 번째 숫자를 입력하세요: ";
+    cin >> secondNumber;
+
+    try {
+        unsigned long long result =
+            static_cast<unsigned long long>(firstNumber) * secondNumber;
+
+        if (result > UINT_MAX)
+            throw UINT_MAX;
+
+        cout << firstNumber << " * " << secondNumber << " = " << result << " 입니다." << endl;
+    } catch (unsigned int max) {
+        cout << "결과값이 unsigned int의 최대값을 벗어납니다." << endl;
+    }
+
+    return 0;
+}
